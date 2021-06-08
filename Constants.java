c98a1ec84c0dbd273ea3e482feea515b81a75a1d
NamrutaBhat <79894256+NamrutaBhat@users.noreply.github.com>
2021-06-08 10:40:27 +0530

    Add files via upload

--- Constants.java
+++ Constants.java
@@ -3,7 +3,7 @@ package com.xworkz.amazon.constants;
 public class Constants {
 	public static final String DRIVER_CLASS_NAME="com.mysql.cj.jdbc.Driver";
 	public static final String URL="jdbc:mysql://localhost:3306/mysql1?user=root&password=vinuvasu";
-	public static final String INSERT_QUERY ="insert into banking_app values(?,?,?,?,?,?,?)";
+	public static final String INSERT_QUERY ="insert into amazon_app values(?,?,?,?,?,?,?)";
 
 public static final String UPDATE_AGE_BY_USER_NAME = "update banking_app set age = ? where user_name= ?";
 public static final String DELETE_USER_BY_ACCOUNT_NO= "delete from banking-app where account_no=?";
